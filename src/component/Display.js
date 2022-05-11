ce16fe41779f0521dcfa6055f7b10373ef1256f0
Aviv <aviv.avra02@gmail.com>
2022-05-11 21:47:21 +0300

    class namings

--- src/component/Display.js
+++ src/component/Display.js
@@ -10,7 +10,7 @@ export default class Display extends React.Component {
 
   render() {
     return (
-      <div className="display-area">
+      <div className="calculator-display">
         <div>{this.props.value}</div>
       </div>
     );
