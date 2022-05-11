2086b713a6fe396de837f9a45ee7ef319a464f0a
Aviv <aviv.avra02@gmail.com>
2022-05-11 21:58:59 +0300

    fixed spacing & sizing issues

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,8 +8,7 @@ export default class Navbar extends React.Component {
             <div className="navbar">
                 <div className="navbar-header">Calculator</div>
                 <div className="navbar-summary">
-                    <button>Saved Calculations</button>
-                    <button>Sign Up</button>
+                    <button className="navbar-button">Saved Calculations</button>
                     <img className="site-logo" src="favicon.ico" alt="Site Logo" />
                 </div>
             </div>
